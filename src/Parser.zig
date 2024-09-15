ac67a0ca290c9bb3054f0c42fd1082b00488fabb
Michele Rossi <mikymod@gmail.com>
2024-09-15 17:20:33 +0200

    pass params "by value"
    https://ziggit.dev/t/whats-the-difference-between-self-mytype-and-self-mytype/2590/2

--- src/Parser.zig
+++ src/Parser.zig
@@ -12,6 +12,33 @@ cur_token: Token = undefined,
 peek_token: Token = undefined,
 errors: std.ArrayList([]const u8),
 
+const ParserError = error{ ExpectExpression, ExpectIdentifier, ExpectPeek, MemoryAllocation };
+
+const ExprPriority = enum(u4) {
+    lowest = 0,
+    equals = 1,
+    less_greater = 2,
+    sum = 3,
+    product = 4,
+    prefix = 5,
+    call = 6,
+
+    fn fromToken(token: Token) ExprPriority {
+        return switch (token) {
+            .EQ => .equals,
+            .NOT_EQ => .equals,
+            .LT => .less_greater,
+            .GT => .less_greater,
+            .PLUS => .sum,
+            .MINUS => .sum,
+            .SLASH => .product,
+            .ASTERISK => .product,
+            .LPAREN => .call,
+            else => .lowest,
+        };
+    }
+};
+
 pub fn init(allocator: Allocator, lexer: *Lexer) Self {
     var parser = Self{
         .allocator = allocator,
@@ -25,26 +52,30 @@ pub fn init(allocator: Allocator, lexer: *Lexer) Self {
     return parser;
 }
 
-pub fn deinit(self: *Self, program: *ast.Program) void {
-    for (program.statements) |stmt| {
-        switch (stmt) {
-            .let => |s| {
-                self.allocator.destroy(s.name);
-                self.allocator.destroy(s);
-            },
-            .@"return" => |s| {
-                self.allocator.destroy(s);
-            },
-            else => unreachable,
-        }
-    }
-    self.allocator.free(program.statements);
-    self.allocator.destroy(program);
+pub fn deinit(self: Self, program: *ast.Program) void {
+    _ = &self;
+    _ = &program;
+    // for (program.statements) |stmt| {
+    //     switch (stmt) {
+    //         .let => |s| {
+    //             self.allocator.destroy(s.name);
+    //             self.allocator.destroy(s);
+    //         },
+    //         .ret => |s| {
+    //             self.allocator.destroy(s);
+    //         },
+    //         .expr => |_| {
+    //             std.debug.print("ExpressionStatement deinit.\n", .{});
+    //         },
+    //     }
+    // }
+    // self.allocator.free(program.statements);
+    // self.allocator.destroy(program);
 
-    for (self.errors.items) |err| {
-        self.allocator.free(err);
-    }
-    self.errors.deinit();
+    // for (self.errors.items) |err| {
+    //     self.allocator.free(err);
+    // }
+    // self.errors.deinit();
 }
 
 pub fn nextToken(self: *Self) void {
@@ -59,9 +90,7 @@ pub fn parseProgram(self: *Self) !*ast.Program {
     var statements = std.ArrayList(ast.Statement).init(self.allocator);
     while (self.cur_token.typez != Token.Type.EOF) {
         const stmt = try self.parseStatement();
-        if (stmt != null) {
-            try statements.append(stmt.?);
-        }
+        try statements.append(stmt);
         self.nextToken();
     }
 
@@ -69,83 +98,119 @@ pub fn parseProgram(self: *Self) !*ast.Program {
     return program;
 }
 
-pub fn parseStatement(self: *Self) !?ast.Statement {
+pub fn parseStatement(self: *Self) ParserError!ast.Statement {
     switch (self.cur_token.typez) {
         Token.Type.LET => {
             const letStmt = try self.parseLetStatement();
-            return .{ .let = letStmt orelse return null };
+            return ast.Statement{ .let = letStmt };
         },
         Token.Type.RETURN => {
             const returnStmt = try self.parseReturnStatement();
-            return .{ .@"return" = returnStmt orelse return null };
+            return ast.Statement{ .ret = returnStmt };
         },
-        else => return null,
+        else => {
+            const exprStmt = try self.parseExpressionStatement();
+            return ast.Statement{ .expr = exprStmt };
+        },
     }
 }
 
-pub fn parseLetStatement(self: *Self) !?*ast.LetStatement {
-    var stmt = try self.allocator.create(ast.LetStatement);
-    errdefer self.allocator.destroy(stmt);
+pub fn parseLetStatement(self: *Self) ParserError!ast.LetStatement {
+    const token = self.cur_token;
 
-    stmt.token = self.cur_token;
+    try self.expectPeek(Token.Type.IDENT);
 
-    if (!self.expectPeek(Token.Type.IDENT)) {
-        self.allocator.destroy(stmt);
-        return null;
-    }
+    const name = ast.Identifier{
+        .token = self.cur_token,
+        .value = self.cur_token.literal,
+    };
 
-    stmt.name = try self.allocator.create(ast.Identifier);
-    errdefer {
-        self.allocator.destroy(stmt.name);
-        self.allocator.destroy(stmt);
-    }
-
-    if (!self.expectPeek(Token.Type.ASSIGN)) {
-        self.allocator.destroy(stmt.name);
-        self.allocator.destroy(stmt);
-        return null;
-    }
-
-    stmt.name.* = .{ .token = self.cur_token, .value = self.cur_token.literal };
-
-    while (!self.curTokenIs(Token.Type.SEMICOLON)) {
-        self.nextToken();
-    }
-
-    return stmt;
-}
-
-pub fn parseReturnStatement(self: *Self) !?*ast.ReturnStatement {
-    const stmt = try self.allocator.create(ast.ReturnStatement);
-    errdefer self.allocator.destroy(stmt);
-
+    try self.expectPeek(Token.Type.ASSIGN);
     self.nextToken();
 
-    // TODO: parse expression
-
-    while (!self.curTokenIs(Token.Type.SEMICOLON)) {
+    const expression = try self.parseExpression(ExprPriority.lowest);
+    if (self.peekTokenIs(Token.Type.SEMICOLON)) {
         self.nextToken();
     }
 
-    return stmt;
+    return ast.LetStatement{
+        .token = token,
+        .name = name,
+        .value = expression,
+    };
 }
 
-pub fn curTokenIs(self: *Self, token_type: Token.Type) bool {
+pub fn parseReturnStatement(self: *Self) !ast.ReturnStatement {
+    self.nextToken();
+
+    const expression = try self.parseExpression(ExprPriority.lowest);
+    if (self.peekTokenIs(Token.Type.SEMICOLON)) {
+        self.nextToken();
+    }
+
+    return ast.ReturnStatement{
+        .token = self.cur_token,
+        .value = expression,
+    };
+}
+
+pub fn parseExpressionStatement(self: *Self) ParserError!ast.ExpressionStatement {
+    const expression = try self.parseExpression(ExprPriority.lowest);
+    // Optional semicolon
+    if (self.peekTokenIs(Token.Type.SEMICOLON)) {
+        self.nextToken();
+    }
+
+    const expressionPtr = self.allocator.create(ast.Expression) catch return ParserError.MemoryAllocation;
+
+    expressionPtr.* = expression;
+
+    return ast.ExpressionStatement{
+        .token = self.cur_token,
+        .expression = expressionPtr,
+    };
+}
+
+pub fn parseExpression(self: Self, _: ExprPriority) ParserError!ast.Expression {
+    const left_exp = try self.parseExpressionByPrefix(self.cur_token);
+
+    // TODO: parse infix expr
+
+    return left_exp;
+}
+
+fn parseExpressionByPrefix(self: Self, token: Token) ParserError!ast.Expression {
+    return switch (token.typez) {
+        Token.Type.IDENT => ast.Expression{ .identifier = try self.parseIdentifier() },
+        else => ParserError.ExpectExpression,
+    };
+}
+
+fn parseIdentifier(self: Self) ParserError!ast.Identifier {
+    return switch (self.cur_token.typez) {
+        Token.Type.IDENT => ast.Identifier{
+            .token = self.cur_token,
+            .value = self.cur_token.literal,
+        },
+        else => ParserError.ExpectIdentifier,
+    };
+}
+
+pub fn curTokenIs(self: Self, token_type: Token.Type) bool {
     return self.cur_token.typez == token_type;
 }
 
-pub fn peekTokenIs(self: *Self, token_type: Token.Type) bool {
+pub fn peekTokenIs(self: Self, token_type: Token.Type) bool {
     return self.peek_token.typez == token_type;
 }
 
-pub fn expectPeek(self: *Self, token_type: Token.Type) bool {
+pub fn expectPeek(self: *Self, token_type: Token.Type) ParserError!void {
     if (self.peekTokenIs(token_type)) {
         self.nextToken();
-        return true;
+    } else {
+        self.peekError(token_type);
+        return ParserError.ExpectPeek;
     }
-
-    self.peekError(token_type);
-    return false;
 }
 
 pub fn peekError(self: *Self, token_type: Token.Type) void {
@@ -216,9 +281,17 @@ test "Parser - Return statement" {
     defer parser.deinit(program);
 
     try std.testing.expect(program.statements.len == 3);
+}
 
-    // for (program.statements) |stmt| {
-    //     try std.testing.expect(std.mem.eql(u8, stmt.@"return".tokenLiteral(), "return"));
-    //     try std.testing.expect(@TypeOf(stmt) == ast.ReturnStatement);
-    // }
+test "Parser - Parse Identifier" {
+    const input = "foobar;";
+
+    var lexer = Lexer.init(input);
+    var parser = init(t.allocator, &lexer);
+    const program = try parser.parseProgram();
+    defer parser.deinit(program);
+
+    try std.testing.expect(program.statements.len == 1);
+
+    // TODO: type checking
 }
--- src/ast.zig
+++ src/ast.zig
@@ -8,7 +8,7 @@ pub const Node = union(enum) {
     statement: *Statement,
     expression: *Expression,
 
-    pub fn toString(self: *Node, allocator: Allocator) []const u8 {
+    pub fn toString(self: Node, allocator: Allocator) []const u8 {
         return switch (self.*) {
             .program => |p| p.toString(allocator),
             .statement => |s| s.toString(allocator),
@@ -19,14 +19,14 @@ pub const Node = union(enum) {
 
 ///
 pub const Statement = union(enum) {
-    let: *LetStatement,
-    @"return": *ReturnStatement,
-    expr: *ExpressionStatement,
+    let: LetStatement,
+    ret: ReturnStatement,
+    expr: ExpressionStatement,
 
-    pub fn toString(self: *const Statement, allocator: Allocator) []const u8 {
-        return switch (self.*) {
+    pub fn toString(self: Statement, allocator: Allocator) []const u8 {
+        return switch (self) {
             .let => |s| s.toString(allocator),
-            .@"return" => |s| s.toString(allocator),
+            .ret => |s| s.toString(allocator),
             .expr => |s| s.toString(allocator),
         };
     }
@@ -34,11 +34,11 @@ pub const Statement = union(enum) {
 
 ///
 pub const Expression = union(enum) {
-    identifier: *Identifier,
+    identifier: Identifier,
 
-    pub fn toString(self: *Expression, allocator: Allocator) []const u8 {
-        return switch (self.*) {
-            .identifier => |s| s.*.toString(allocator),
+    pub fn toString(self: Expression, _: Allocator) []const u8 {
+        return switch (self) {
+            .identifier => |s| s.toString(),
             // else => unreachable,
         };
     }
@@ -48,11 +48,11 @@ pub const Identifier = struct {
     token: Token,
     value: []const u8,
 
-    pub fn tokenLiteral(self: *Identifier) []const u8 {
+    pub fn tokenLiteral(self: Identifier) []const u8 {
         return self.token.literal;
     }
 
-    pub fn toString(self: *Identifier, _: Allocator) []const u8 {
+    pub fn toString(self: Identifier) []const u8 {
         return self.value;
     }
 };
@@ -60,35 +60,35 @@ pub const Identifier = struct {
 pub const Program = struct {
     statements: []Statement,
 
-    pub fn tokenLiteral(p: *Program) []const u8 {
+    pub fn tokenLiteral(p: Program) []const u8 {
         if (p.statements.len > 0) {
             return p.statements[0].tokenLiteral();
         }
         return "";
     }
 
-    pub fn toString(self: *Program, allocator: Allocator) []const u8 {
-        var buf = std.ArrayList(u8).initCapacity(allocator, 32) catch unreachable;
+    pub fn toString(self: Program, allocator: Allocator) ![]const u8 {
+        var buf = std.ArrayList(u8).init(allocator);
         defer buf.deinit();
         for (self.statements) |stmt| {
             const stmt_str = stmt.toString(allocator);
             defer allocator.free(stmt_str);
-            buf.appendSlice(stmt_str) catch unreachable;
+            try buf.appendSlice(stmt_str);
         }
-        return buf.toOwnedSlice() catch unreachable;
+        return buf.toOwnedSlice();
     }
 };
 
 pub const LetStatement = struct {
     token: Token,
-    name: *Identifier,
-    value: *Expression,
+    name: Identifier,
+    value: Expression,
 
-    pub fn tokenLiteral(self: *LetStatement) []const u8 {
+    pub fn tokenLiteral(self: LetStatement) []const u8 {
         return self.token.literal;
     }
 
-    pub fn toString(self: *LetStatement, allocator: Allocator) []const u8 {
+    pub fn toString(self: LetStatement, allocator: Allocator) []const u8 {
         return std.fmt.allocPrint(allocator, "{s} {s} = {s};", .{
             self.tokenLiteral(),
             self.name.tokenLiteral(),
@@ -99,17 +99,13 @@ pub const LetStatement = struct {
 
 pub const ReturnStatement = struct {
     token: Token,
-    value: *Expression,
+    value: Expression,
 
-    pub fn node(self: *ReturnStatement) Node {
-        return Node.init(self);
-    }
-
-    pub fn tokenLiteral(self: *ReturnStatement) []const u8 {
+    pub fn tokenLiteral(self: ReturnStatement) []const u8 {
         return self.token.literal;
     }
 
-    pub fn toString(self: *ReturnStatement, allocator: Allocator) []const u8 {
+    pub fn toString(self: ReturnStatement, allocator: Allocator) []const u8 {
         const str = self.value.toString(allocator);
         defer allocator.free(str);
         return std.fmt.allocPrint(allocator, "{s} {s};", .{ self.tokenLiteral(), str }) catch unreachable;
@@ -120,19 +116,18 @@ pub const ExpressionStatement = struct {
     token: Token,
     expression: *Expression,
 
-    pub fn tokenLiteral(self: *ExpressionStatement) []const u8 {
+    pub fn tokenLiteral(self: ExpressionStatement) []const u8 {
         return self.token.literal;
     }
 
-    pub fn toString(self: *ExpressionStatement, allocator: Allocator) []const u8 {
+    pub fn toString(self: ExpressionStatement, allocator: Allocator) []const u8 {
         const str = self.expression.toString(allocator);
         return str;
     }
 };
 
 test "AST - toString" {
-    // TODO: write test for toString
-    var name_ident = Identifier{
+    const name_ident = Identifier{
         .token = Token{
             .typez = Token.Type.IDENT,
             .literal = "myVar",
@@ -140,7 +135,9 @@ test "AST - toString" {
         .value = "myVar",
     };
 
-    var value_ident = Identifier{
+    _ = &name_ident;
+
+    const value_ident = Identifier{
         .token = Token{
             .typez = Token.Type.IDENT,
             .literal = "anotherVar",
@@ -148,19 +145,21 @@ test "AST - toString" {
         .value = "anotherVar",
     };
 
-    var expr = Expression{ .identifier = &value_ident };
+    const expr = Expression{ .identifier = value_ident };
 
     var let = LetStatement{
         .token = Token{ .typez = Token.Type.LET, .literal = "let" },
-        .name = &name_ident,
-        .value = &expr,
+        .name = name_ident,
+        .value = expr,
     };
+    _ = &let;
 
     var statements = [_]Statement{
-        .{ .let = &let },
+        .{ .let = let },
     };
 
-    var program = Program{ .statements = &statements };
+    const program = Program{ .statements = &statements };
+    _ = &program;
 
-    try std.testing.expect(std.mem.eql(u8, program.toString(std.testing.allocator), "let myVar = anotherVar;"));
+    try std.testing.expect(std.mem.eql(u8, try program.toString(std.testing.allocator), "let myVar = anotherVar;"));
 }
